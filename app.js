2ae17d9bc195043f873a340bcbe7948098c8d9e6
Tamiko Terada <tamikodevo@gmail.com>
2017-07-09 22:05:08 -0700

    Create Express web server with socket.
    Add dependencies - express and socket.io.
    Set up express app as a socket server with callback.

---
+++ app.js
@@ -0,0 +1,19 @@
+// dependencies
+var express = require('express');
+var socket = require('socket.io');
+
+// set up app on part :3000
+var app = express();
+var server = app.listen(3000, function(){
+  console.log('listening to local requests on :3000');
+});
+
+// location of static files
+app.use(express.static('public'));
+
+// set up socket server
+var io = socket(server);
+
+io.on('connection', function(socket) {
+  console.log('socket created! connected to ' + socket.id);
+});
