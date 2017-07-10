e7021ab6a11a0ee659e5345cb388f084eee2ebce
Tamiko Terada <tamikodevo@gmail.com>
2017-07-10 11:40:44 -0700

    Can see basic chat func in browser.
    Message event listener + handler added on client-side.
    Emit message handler added on server-side.
    Fixed typo in app.js.

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 var express = require('express');
 var socket = require('socket.io');
 
-// set up app on part :3000
+// set up app on port :3000
 var app = express();
 var server = app.listen(3000, function(){
   console.log('listening to local requests on :3000');
@@ -16,4 +16,11 @@ var io = socket(server);
 
 io.on('connection', function(socket) {
   console.log('~socket made: connected to ' + socket.id);
+
+  // triggered from client side with button
+  socket.on('chat', function(data){
+  // sending data back to ALL sockets
+    io.sockets.emit('chat', data);
+  });
+
 });
--- public/chat.js
+++ public/chat.js
@@ -1,3 +1,24 @@
 // make client-side connection with Express server
-
 var socket = io.connect('http://localhost:3000');
+
+// raw DOM objects as variables
+var message = document.getElementById('message'),
+    handle = document.getElementById('username'),
+    sendButton = document.getElementById('send'),
+    messages = document.getElementById('messages');
+
+// event triggered w/ button click - emit to Express server
+sendButton.addEventListener('click', function(){
+  console.log(message.value);
+  socket.emit('chat', {
+    'message': message.value,
+    'username': username.value
+  });
+  // reset message content
+  message.value = "";
+});
+
+// event handler to add messages to DOM
+socket.on('chat', function(data){
+  messages.innerHTML += '<li>' + data.username + ': ' + data.message + '</li>';
+});
