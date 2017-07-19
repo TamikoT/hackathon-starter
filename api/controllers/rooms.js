511aac46a01860b81525cd6311bf23518447bf3d
Tamiko Terada <tamikodevo@gmail.com>
2017-07-18 17:21:36 -0700

    add update function to roomsController

--- api/controllers/rooms.js
+++ api/controllers/rooms.js
@@ -5,7 +5,7 @@ var bodyParser = require('body-parser');
 router.use(bodyParser.urlencoded({ extended: true }));
 var Room = require('../models/Room');
 
-// INDEX: returns all users in db
+// INDEX: returns all rooms in db
 router.get('/', function (req, res) {
   Room.find({}, function (err, rooms) {
     if (err) return res.status(500).send("There was a problem finding the rooms.");
@@ -13,12 +13,11 @@ router.get('/', function (req, res) {
   });
 });
 
-// NEW: creates a new user in db
+// NEW: creates a new room in db
 router.post('/', function (req, res) {
   Room.create({
-    username : req.body.username,
-    isHost : req.body.isHost,
-    voteCount : req.body.voteCount
+    code : req.body.code,
+    _hostID : req.body.isHost,
   },
   function (err, room) {
     if (err) return res.status(500).send("There was a problem adding the information to the database.");
@@ -26,20 +25,29 @@ router.post('/', function (req, res) {
   });
 });
 
-// ROOM: find one by id
+// SHOW: find one room by id
 router.get('/:id', function (req, res) {
-    Room.findById(req.params.id, function (err, user) {
-        if (err) return res.status(500).send("There was a problem finding the room.");
-        if (!room) return res.status(404).send("No room found.");
-        res.status(200).send(room);
-    });
+  Room.findById(req.params.id, function (err, room) {
+    if (err) return res.status(500).send("There was a problem finding the room.");
+    if (!room) return res.status(404).send("No room found.");
+    res.status(200).send(room);
+  });
 });
 
+// UPDATE: edit an existing room
+router.put('/:id', function (req, res) {
+  Room.findByIdAndUpdate(req.params.id, req.body, {new: true}, function (err, room) {
+    if (err) return res.status(500).send("There was a problem updating the room.");
+    res.status(200).send(room);
+  });
+});
+
+// DELETE: delete one room by id
 router.delete('/:id', function (req, res) {
-    Room.findByIdAndRemove(req.params.id, function (err, user) {
-        if (err) return res.status(500).send("There was a problem deleting the room.");
-        res.status(200).send("Room "+ Room.code +" was deleted.");
-    });
+  Room.findByIdAndRemove(req.params.id, function (err, room) {
+    if (err) return res.status(500).send("There was a problem deleting the room.");
+    res.status(200).send("Room "+ Room.code +" was deleted.");
+  });
 });
 
 module.exports = router;
