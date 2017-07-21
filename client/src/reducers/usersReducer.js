c7258ff5e870af8a1d923d3a9f2801c63b25cab8
Tamiko Terada <tamikodevo@gmail.com>
2017-07-21 15:25:49 -0700

    Change React state based on user input on Home Page
    Set value and create event handler on each input.
    Update state to reflect input fields in constructor.

--- client/src/reducers/usersReducer.js
+++ client/src/reducers/usersReducer.js
@@ -1,6 +1,6 @@
 import initialState from './initialState'
 
-export default function usersReducer(state='initialState.users', action){
+export default function usersReducer(state=initialState.users, action){
   switch(action.type){
     case 'NEW-USER':
       var newState = action.user;
