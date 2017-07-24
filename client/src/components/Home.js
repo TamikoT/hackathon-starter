4f877a5247bae3dcb0f56b9898cacb8f9108a989
Tamiko Terada <tamikodevo@gmail.com>
2017-07-23 19:19:37 -0700

    revert changes on Home component because of errors

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
-import * as userActions from '../actions/userActions';
-import ChatWindow from './ChatWindow';
-import { Field, reduxForm } from 'redux-form';
+import * as userActions from '../actions/userActions'
 
 class Home extends Component {
   constructor(props){
@@ -11,8 +9,7 @@ class Home extends Component {
     this.handleOnClick = this.handleOnClick.bind(this);
     this.state = {
       username: '',
-      code: '',
-      isLoggedIn: false
+      code: ''
     };
   }
 
@@ -31,53 +28,43 @@ class Home extends Component {
     }).then(function(response) { return response.json(); } )
       .catch(function(err) { "something went wrong"; });
 
-    // redirect to '/chat'
-    // window.location.replace('/chat');
-
-
     // action to create a new user
     this.props.newUser(this.state.username);
 
-    // reset username and room code
+    // add user to a room
     this.setState({ username: '', code: ''});
 
-    this.setState({isLoggedIn: true});
+    // redirect to '/chat'
+    window.location.replace('/chat');
   }
 
   render() {
-    const isLoggedIn = this.state.isLoggedIn;
     return (
-      <div>
-        { isLoggedIn ? (
-          <section className="home-component">
-            <p className="console-log">inside home</p>
-            <h2>Welcome to Muviato!</h2>
-            <ul>
-              <li>
-                <label>your sweet username: </label>
-                <input type="text" placeholder="make a username" value={ this.state.username }
-           onChange={(e) => this.setState({ username: e.target.value })} id="username" />
-              </li>
-              <li>
-                <label>the secret code, please: </label>
-                <input type="text" placeholder="room code" value={ this.state.code }
-           onChange={(e) => this.setState({ code: e.target.value })} id="roomCode" />
-                <button onClick={ this.handleOnClick }>Enter</button>
-              </li>
-            </ul>
-            <div>
-              <Link to='/host'>create a new room</Link>
-            </div>
-          </section>
-        ) : (
-          <p>User is logged in!</p>
-        ) }
-      <div>
+      <section className="home-component">
+        <p className="console-log">inside home</p>
+        <h2>Welcome to Muviato!</h2>
+        <ul>
+          <li>
+            <label>your sweet username: </label>
+            <input type="text" placeholder="make a username" value={this.state.username}
+       onChange={(e) => this.setState({ username: e.target.value })} id="username" />
+          </li>
+          <li>
+            <label>the secret code, please: </label>
+            <input type="text" placeholder="room code" value={this.state.code}
+       onChange={(e) => this.setState({ code: e.target.value })} id="roomCode" />
+            <Link to='/user' onClick={this.handleOnClick}><button>Enter</button></Link>
+          </li>
+        </ul>
+        <div>
+          <Link to='/host'>create a new room</Link>
+        </div>
+      </section>
     )
   }
 }
 
-function mapStateToProps(state){
+function mapStateToProps(state, ownProps){
   return { user: state.user }
 }
 
@@ -89,4 +76,4 @@ function mapDispatchToProps(dispatch){
  }
 }
 
-export default connect(null, mapDispatchToProps)(Home);
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
