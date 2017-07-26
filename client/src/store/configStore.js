7987519443c0312e6d2ca3298a947f8bcb2ea04b
Tamiko Terada <tamikodevo@gmail.com>
2017-07-26 03:28:08 -0700

    add initial state to rootReducer

--- client/src/store/configStore.js
+++ client/src/store/configStore.js
@@ -1,6 +1,7 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk'; // returns a function instead of an action - used to delay an action
 import RootReducer from '../reducers/rootReducer';
+import initialState from '../reducers/initialState';
 
 // getState() - returns state of current app
 // dispatch(action) - triggers a state change
@@ -9,6 +10,7 @@ import RootReducer from '../reducers/rootReducer';
 export default () => {
   return createStore (
     RootReducer,
+    initialState,
     applyMiddleware(thunk)
   );
 };
