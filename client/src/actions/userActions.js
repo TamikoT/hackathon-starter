2bcf488a6abf452738d9db4e22a43bf36d6e7b3a
Tamiko Terada <tamikodevo@gmail.com>
2017-07-26 11:41:16 -0700

    WIP :: add newUser action with API call

--- client/src/actions/userActions.js
+++ client/src/actions/userActions.js
@@ -1,6 +1,27 @@
 export const NEW_USER = 'NEW_USER'
 
-export function newUser(user){
-  // console.log(user);
-  return { type: NEW_USER, user }
+const API_USERS_URL = 'http://localhost:3001/api/users';
+
+export const NEW_USER = 'NEW_USER';
+
+export function newUser(username){
+  console.log(`called newUser() w/username: (${username})`);
+  return(dispatch) => {
+    var userData = { username };
+    axios.post(API_USERS_URL, userData)
+      .then( (res) => {
+        console.log(res);
+        dispatch(newUserSuccess(codeData));
+      })
+      .catch(function (err) {
+        console.log(err);
+        alert(err);
+      });
+  };
+}
+
+export function newUserSuccess(payload) {
+  alert('New user created!');
+  console.log(`called newUserSuccess() w/payload: ${payload}`);
+  return { type: NEW_USER, payload };
 }
