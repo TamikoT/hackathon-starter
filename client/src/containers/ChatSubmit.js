60bb663f2487e2bf4b35f25c0452a4ad3367981e
Tamiko Terada <tamikodevo@gmail.com>
2017-07-27 14:44:22 -0700

    clear up ChatSubmit and connect with socket.io

--- client/src/containers/ChatSubmit.js
+++ client/src/containers/ChatSubmit.js
@@ -5,28 +5,23 @@ import { connect } from 'react-redux';
 import Header from '../components/Header';
 import * as roomActions from '../actions/roomActions';
 import * as userActions from '../actions/userActions';
-import io from 'socket.io-client';
+import * as messageActions from '../actions/messageActions';
 
+import io from 'socket.io-client';
 const socket = io('http://localhost:3001');
 
 class ChatSubmit extends Component {
   constructor(props) {
-    super (props);
+    super(props);
     console.log(props);
-
     console.log(socket);
 
     this.state = { currentUser: props.currentUser };
-    console.log( this.state );
-
-    // if ( this.state.username === undefined ) {
-    //   console.log( 'username and code are both needed' );
-    //   this.props.history.push( '/' );
-    // }
+    console.log(this.state);
   }
 
+  // redux-form: event handlers for fields
   renderField(field) {
-    // adds event handlers for fields
     return (
       <div className={`form-group ${field.meta.touched && field.meta.invalid ? 'has-danger' : ''}`}>
         <label className="control-label">{field.label}</label>
@@ -54,14 +49,13 @@ class ChatSubmit extends Component {
   }
 
   onSubmit(formData) {
-    console.log('in onSubmit');
-    console.log(this.props);
-    this.props.addMessage(formData);
+    console.log(formData);
+    this.addMessage(formData);
 
     // emit event to server to join a room
     socket.emit('enterRoom', {
       'code': 'ADA1',
-      'username': this.state.username
+      'username': this.state.currentUser.username
     });
     // w/ callback for server response
     socket.on('enterRoom', (data) => {
@@ -69,32 +63,24 @@ class ChatSubmit extends Component {
     });
 
     socket.emit('msgSent', {
-      'username': this.state.username,
+      'username': this.state.currentUser.username,
       'message': formData.message
     })
-
     socket.on('msgShared', (data) => {
       console.log(data);
-      // attach message to DOM
-      // use setState
-
-      // ReactDOM.render(
-      //   // this.msgComponent(data),
-      //   document.getElementById('messages-container')
-      // );
+      this.render();
     });
-
   }
 
   // `handleSubmit()` is a redux-form func for validations
   render() {
-    const { handleSubmit } = this.props;
+    const {handleSubmit} = this.props;
     return (
       <section>
         <Header />
         <form onSubmit={handleSubmit(this.onSubmit)}>
-          <h3>now in room: {this.state.code}</h3>
-          <h4>as {this.state.username}</h4>
+          <h3>now in room: {this.state.currentUser.code}</h3>
+          <h4>as {this.state.currentUser.username}</h4>
           <Field
             name='message'
             label='message: '
@@ -117,18 +103,17 @@ function validate(values) {
   return errors;
 }
 
-
 function mapStateToProps(state, ownProps) {
   return { currentUser: state.currentUser }
 }
 
 const mapDispatchToProps = (dispatch) => {
-  return bindActionCreators({ createRoom: roomActions.createRoom, newUser: userActions.newUser }, dispatch)
+  return bindActionCreators({addMessage: messageActions.addMessage}, dispatch)
 }
 
-export default reduxForm({
+export default reduxForm( {
   form: 'ChatSubmit',
   validate
-})(
+} )(
   connect(mapStateToProps, mapDispatchToProps)(ChatSubmit)
 );
